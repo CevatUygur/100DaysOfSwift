36980606047347830115ae917b191d328a0d0758
CevatUygur <cevatuygur@gmail.com>
2022-04-25 19:07:34 +0300

    First Commit

---
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by CEVAT UYGUR on 25.04.2022.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+}
+
