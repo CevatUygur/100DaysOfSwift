f6fdffdc336458e2dd22469056ecb38cfd8eaa98
CevatUygur <cevatuygur@gmail.com>
2022-04-26 23:59:39 +0300

    Credits button added

--- WhitehousePetitions/WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Whitehouse Petitions"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showCredits))
+        
         let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
@@ -30,6 +34,12 @@ class ViewController: UITableViewController {
         }
         
         showError()
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "Shown data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func showError() {
