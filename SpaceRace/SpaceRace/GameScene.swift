533ed9f11dd4b7066fa3c594bdfce114b752a3f6
CevatUygur <cevatuygur@gmail.com>
2022-06-06 13:17:15 +0300

    First Commit

---
+++ SpaceRace/SpaceRace/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by CEVAT UYGUR on 6.06.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
